bfc6fa826a956ffd67fa0cd935729e35d6f71b26
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-07-19 15:59:25 -0600

    Update API_use_v2.py

--- API_use_v2.py
+++ API_use_v2.py
@@ -5,43 +5,34 @@ import json
 # Find out starting and ending locations
 # Obtain the directions from google API and save locally
 base_url = 'https://maps.googleapis.com/maps/api/directions/json?'
-origin_base = 'origin=' #Location you want to start from
-origin= '1391EmpireRd'
-filler1 = '&'
+origin_base = 'origin='                     #Location you want to start from
+origin= '1391EmpireRd'                      #Starting location (for now)
+filler1 = '&'                               #Filler for google API syntax
 destination_base = 'destination='
-destination= 'GatewayParkFunCenter'
+destination= 'GatewayParkFunCenter'         #Destination
 key = 'key=AIzaSyCbWbe5KEMSV0UY3HJ5BEppCIUyp8Z9Brs'
 
 url = base_url+origin_base+origin+filler1+destination_base+destination+filler1+key
 print(url)
 result = urllib.request.urlopen(url, data=None, )
 # Result= address of data, File= Contents of url
-json_file = result.read()
-# print(json_file)
-route_data = json.loads(json_file)
+json_file = result.read()                   #Read JSON into Python
+route_data = json.loads(json_file)          #Take out excess JSON crap that clogs up the screen (makes it easier for user to read)
 
-lat = 'lat'
-lng = 'lng'
-space = ' '
-endpoints= []
-startpoints= []
-maneuvers= []
-holder=[]
-# for word in  route_data:
-#    if delimiter1 in route_data:
-#        z=1
-# pass
 
-routes = route_data['routes']
-# print(type(routes[0]))  Is a dict
-# print(type(routes[0]['legs'][0]['steps']))  Is a list
-steps = routes[0]['legs'][0]['steps'] # Is a list
-# print(steps)
+endpoints= []                               #Array for storing lat and lng of startpoints
+startpoints= []                             #Array for storing lat and lng for endpoints
+maneuvers= []                               #Array for storing maneuvers
+holder=[]                                   #Array used to fill the startpoint and endpoint arrays
+
+
+routes = route_data['routes']               #Cuts out unnecessary JSON data
+steps = routes[0]['legs'][0]['steps']
 iterations = len(steps)
 
-# instructions_dict={latitude: , longitude: , maneuver: }
 
-for i in range(iterations):
+
+for i in range(iterations):                 #This loop fills in the arrays above with the relevant data that is parsed from the JSON file
     holder = steps[i]['end_location']
     endpoints.append(holder)
     holder = steps[i]['start_location']
@@ -55,22 +46,13 @@ for i in range(iterations):
 print('startpoints= ', startpoints)
 print('endpoints= ', endpoints)
 print('maneuvers =' , maneuvers)
-print(endpoints[1]['lat'])
-
-new_dict=dict()
-new_dict['lat']= endpoints[0]['lat']
-
-print(new_dict)
-i=0
-#for i in range(iterations):
-#   instructions_dict.updateendpoints[i]['lat']
-#   endpoints[i]['lng']
-#   maneuvers[i]
 
 
-# Attempt to create data structure
 
-class Instruction:
+
+
+
+class Instruction:                          # Data structure to organize the lat, lng, and maneuver data into one instruction
     def __init__(self, lat, lng, maneuver):
         self.lat = lat
         self.lng = lng
@@ -78,5 +60,10 @@ class Instruction:
 
 
 
-
+directions=[]                               # Array of organized directions
+for i in range(iterations):                 # This loop fills in the directions array
+    directions.append(Instruction(endpoints[i]['lat'], endpoints[i]['lng'], maneuvers[i]))
+    print(directions[i].lat)
+    print(directions[i].lng)
+    print(directions[i].maneuver)
 
